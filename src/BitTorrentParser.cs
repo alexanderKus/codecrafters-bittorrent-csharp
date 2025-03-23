880f3332134b0f7f4a7123d863daa39b9ac79e4d
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-03-23 18:12:18 +0100

    Task9Logs5

--- src/BitTorrentParser.cs
+++ src/BitTorrentParser.cs
@@ -71,7 +71,7 @@ public sealed class BitTorrentParser
     
     private BitTorrentByteArray ParseByteArray(ReadOnlySpan<char> data)
     {
-        Console.WriteLine($"xxx: {data.Length == _bytes.Length}");
+        Console.WriteLine($"xxx: {data.Length == _bytes.Length}, {data.Length}, {_bytes.Length}");
         var s = _index;
         AdvanceTill(':', data);
         var len = int.Parse(data[s.._index]);
