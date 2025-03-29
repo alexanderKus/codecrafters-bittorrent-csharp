ec1337cce2a7f41c29122339296ff64f2a1f086b
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-03-29 19:27:23 +0100

    Task11Fix

--- src/Program.cs
+++ src/Program.cs
@@ -136,16 +136,15 @@ else if (command == "download_piece")
         stream.Read(unchokeBuffer);
         Console.WriteLine($"UnchokeBuffer: {Convert.ToHexString(unchokeBuffer).ToLower()}");
         List<byte> piece = new();
-        var len = Math.Min(info.Info.Length ?? 0 - pieceIndex * info.Info.PieceLength,  info.Info.PieceLength);
-        for (var i = 0; i < len/16384; i++)
+        for (var i = 0; i < 16; i++)
         {
-            var len2 = (i == len/16384-1 ? info!.Info!.Length % info!.Info!.PieceLength : info!.Info!.PieceLength) ?? throw new Exception("Cannot calculate length of a piece");
+            var len = (i == 15 ? info!.Info!.Length % info!.Info!.PieceLength : info!.Info!.PieceLength) ?? throw new Exception("Cannot calculate length of a piece");
             var requestBuffer = Array.Empty<byte>()
                 .Concat(new byte [] {0,0,0,13})
                 .Append((byte)BitTorrentMessageType.Request)
                 .Append((byte)0x0).Append((byte)0x0).Append((byte)0x0).Append(pieceIndex)
                 .Concat(BitConverter.GetBytes(i*16384))
-                .Concat(BitConverter.GetBytes((int)len2))
+                .Concat(BitConverter.GetBytes((int)len))
                 .ToArray();
             Console.WriteLine($"RequestBuffer id:{i}: {Convert.ToHexString(requestBuffer).ToLower()}");
             stream.Write(requestBuffer);
@@ -169,7 +168,7 @@ else if (command == "download_piece")
         }
         Console.WriteLine($"Piece Hash: {Convert.ToHexString(pieceHash).ToLower()}");
         Console.WriteLine($"Piece {Convert.ToHexString(piece.ToArray()).ToLower()}");
-        //outputFile.Write(piece.ToArray());
+        outputFile.Write(piece.ToArray());
     }
 }
 else
