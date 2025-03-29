914ec44bf5940614d2aacfaa4011545fa60e7de4
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-03-29 20:58:15 +0100

    Task11Fix

--- src/Program.cs
+++ src/Program.cs
@@ -158,8 +158,9 @@ else if (command == "download_piece")
             stream.Write(requestBuffer);
             var pieceLenBuffer = new byte[4];
             stream.Read(pieceLenBuffer, 0 ,pieceLenBuffer.Length);
-            var pieceLen = BitConverter.ToInt32(pieceLenBuffer.Reverse().ToArray(), 0); //BinaryPrimitives.ReadInt32BigEndian(pieceLenBuffer);
+            var pieceLen = BitConverter.ToInt32(pieceLenBuffer.Reverse().ToArray(), 0);
             Console.WriteLine($"--->Reading pieceLen {pieceLen}");
+            if (pieceLen == 0) break;
             stream.ReadByte(); // messageId
             var pieceBuffer = new byte[pieceLen-1];
             stream.Read(pieceBuffer, 0 ,pieceBuffer.Length);
