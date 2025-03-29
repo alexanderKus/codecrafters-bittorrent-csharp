e5b490c90d7512932cb60c9849a64fa51c352ca1
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2025-03-29 18:25:48 +0100

    Task11Fix

--- src/Program.cs
+++ src/Program.cs
@@ -148,7 +148,7 @@ else if (command == "download_piece")
             Console.WriteLine($"RequestBuffer id:{i}: {Convert.ToHexString(requestBuffer).ToLower()}");
             stream.Write(requestBuffer);
             
-            var pieceBuffer = new byte[(2^14+13)];
+            var pieceBuffer = new byte[16384];
             stream.Read(pieceBuffer);
             Console.WriteLine($"PieceBuffer: {Convert.ToHexString(pieceBuffer).ToLower()}");
             piece.AddRange(pieceBuffer[13..].ToArray());
